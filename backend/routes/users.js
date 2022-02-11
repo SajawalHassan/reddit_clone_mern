bc39a566daac758758a5e8a71a20aa0dd716260e
SajawalHassan <scientificninja1f12@gmail.com>
2022-02-11 21:46:07 +0500

    Add route to edit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const { verify } = require("../authentication/verifyToken");
 
@@ -9,6 +10,25 @@ router.get("/profile", verify, async (req, res) => {
     const user = await User.findById(req.user._id);
 
     res.json(user);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+router.put("/edit", verify, async (req, res) => {
+  try {
+    // Hashing password
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+
+    // Updating user
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, {
+      $set: req.body,
+    });
+
+    res.json(updatedUser);
   } catch (err) {
     res.status(404).json(err);
   }
