0ba82913f1bf374dd0cd3c146e61990d8dd248c3
SajawalHassan <scientificninja1f12@gmail.com>
2022-02-11 21:12:26 +0500

    Add verification

---
+++ backend/utils/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.verify = function (req, res, next) {
+  try {
+    // Checking if the user has the token
+    const token = req.header("auth-token");
+    // If he doesn't then throw err
+    if (!token) {
+      return res.status(400).json("Please sign in/up first!");
+    }
+
+    // Verifying token
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
