6dd79851a24e455a36ec28779944bb28cd8353b2
SajawalHassan <scientificninja1f12@gmail.com>
2022-02-20 09:22:07 +0500

    Add comments

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -4,34 +4,36 @@ import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { register } from "../state/actions/auth";
 
 function Register() {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const error = useSelector((state) => state.error);
+  const registerUser = useSelector((state) => state.register);
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
 
-  const error = useSelector((state) => state.error);
-  const registerUser = useSelector((state) => state.register);
-
   const handleOnClick = (e) => {
     e.preventDefault();
 
+    // Dispatching an action
     dispatch(register(username, email, password));
 
+    // Pushing user to the login page
     if (registerUser.redirect) {
       history.push("/login");
     }
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-[#f59d7d]">
-      <div className="shadow-2xl px-4 pb-8 pt-3 mx-1 bg-orange-200 rounded-md max-w-md md:max-w-lg lg:max-w-2xl">
+    <div className="h-screen flex items-center justify-center bg-gradient-to-bl from-[#f59d7d] to-[#fab8a0]">
+      <div className="shadow-2xl px-4 pb-3 pt-3 mx-1 bg-orange-200 rounded-md max-w-md md:max-w-lg lg:max-w-2xl">
         <h1 className="text-2xl uppercase">signup</h1>
         <p className="text-gray-600 text-sm mt-2">Welcome to Reddit!</p>
 
@@ -93,12 +95,21 @@ function Register() {
 
           <button
             type="submit"
-            className="bg-blue-500 py-2 w-[70%] rounded-full text-center mx-14 md:mx-15 lg:mx-16"
+            className="bg-blue-500 py-2 w-[70%] rounded-full text-center mx-14 md:mx-15 lg:mx-16 hover:bg-blue-600 transition-colors duration-300"
             onClick={handleOnClick}
           >
             <h1 className="font-bold text-white">Register</h1>
           </button>
         </form>
+        <h1 className="text-xs text-slate-600 mt-4 text-center">
+          Already a redditor?{" "}
+          <Link
+            to="/login"
+            className="font-bold text-blue-500 text-xs hover:underline"
+          >
+            LOG IN
+          </Link>
+        </h1>
       </div>
     </div>
   );
