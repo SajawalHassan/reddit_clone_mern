4b841626585005f476a8e6a08dc56a3342c556e3
SajawalHassan <scientificninja1f12@gmail.com>
2022-03-25 18:40:04 +0500

    Add votes to comments

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -68,11 +68,60 @@ router.post("/repost/:id", authenticate, async (req, res) => {
       ownerId: req.user._id,
     });
 
-    // Saving post
+    // Saving comment
     await newComment.save();
 
     res.json(newComment);
   } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.put("/upvote/:id", authenticate, async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    // Checking if the comment isn't already upvoted
+    if (!comment.upvotes.includes(req.user._id)) {
+      // Adding upvote
+      await comment.updateOne({
+        $push: { upvotes: req.user._id },
+        $pull: { downvotes: req.user._id },
+      });
+
+      return res.json("comment upvoted");
+    } else {
+      // Removing upvote
+      await comment.updateOne({ $pull: { upvotes: req.user._id } });
+
+      return res.json("Upvote removed");
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.put("/downvote/:id", authenticate, async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    // Checking if the comment isn't already downvoted
+    if (!comment.downvotes.includes(req.user._id)) {
+      // Adding downvote
+      await comment.updateOne({
+        $push: { downvotes: req.user._id },
+        $pull: { upvotes: req.user._id },
+      });
+
+      return res.json("comment downvoted");
+    } else {
+      // Removing downvote
+      await comment.updateOne({ $pull: { downvotes: req.user._id } });
+
+      return res.json("Downvote removed");
+    }
+  } catch (error) {
+    console.log(error);
     res.sendStatus(500);
   }
 });
