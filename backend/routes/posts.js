7547e341b4346fe0034ffc2cc4149f484f636128
SajawalHassan <scientificninja1f12@gmail.com>
2022-03-25 17:37:13 +0500

    Add route to repost post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -34,9 +34,11 @@ router.put("/edit/:id", authenticate, async (req, res) => {
     // Finding post
     const post = await Post.findById(req.params.id);
 
+    // Making sure the user is the owner
     if (post.ownerId !== req.user._id)
       return res.status(403).json("You are not the owner!");
 
+    // Editing post
     const editedPost = await post.updateOne({ $set: req.body });
 
     res.json(editedPost);
@@ -47,16 +49,43 @@ router.put("/edit/:id", authenticate, async (req, res) => {
 
 router.delete("/delete/:id", authenticate, async (req, res) => {
   try {
+    // Finding post
     const post = await Post.findById(req.params.id);
 
+    // Making sure the user is the owner
     if (post.ownerId !== req.user._id)
       return res.status(403).json("You are not the owner!");
 
+    // Deleting post
     await post.deleteOne();
 
     res.json("Post deleted");
   } catch (error) {
     res.status(500);
+  }
+});
+
+router.post("/repost/:id", authenticate, async (req, res) => {
+  try {
+    // Finding post
+    const post = await Post.findById(req.params.id);
+
+    // Getting info for new post
+    const newPost = new Post({
+      title: post.title,
+      wordContent: post.wordContent,
+      picContent: post.picContent,
+      subredditId: post.subredditId,
+      subredditPic: post.subredditPic,
+      ownerId: req.user._id,
+    });
+
+    // Saving new post
+    await newPost.save();
+
+    res.json("Post reposted");
+  } catch (error) {
+    res.sendStatus(500);
   }
 });
 
